b272fb1f828f32f36edd25cf371a4440ac96d2f4
Nicola <nicolusso@gmail.com>
2020-12-21 16:36:36 +0100

    ordinato

--- js/script.js
+++ js/script.js
@@ -3,13 +3,11 @@ var numeroUtente,numeroGenerato, scelta;
 scelta = prompt('Pari o dispari?')
 console.log(scelta);
 
-numeroUtente = prompt('Inserisci un numero da 1 a 5');
+numeroUtente = parseInt(prompt('Inserisci un numero da 1 a 5'));
 console.log(numeroUtente);
 
 numeroGenerato = generatoreRandom(1,5);
 console.log(numeroGenerato);
-
-
 
 
 function generatoreRandom(min, max) {
@@ -19,11 +17,26 @@ function generatoreRandom(min, max) {
 
 }
 
+var somma = numeroUtente + numeroGenerato;
+console.log(somma);
+
+if (scelta == risultato) {
+  console.log('hai vinto!');
+} else {
+  console.log('ha vinto il computer');
+}
+
+var risultato = pariDispari(numeroUtente, numeroGenerato);
+// risultato = true;
+console.log(risultato);
+
+
+
 function pariDispari(numeroUtente, numeroGenerato) {
-  var somma = numeroUtente + numeroGenerato;
+
   if (somma % 2 == 0) {
-    return true;
+    return console.log('La somma è pari') ;
   } else {
-    return false;
+    return console.log('La somma è dispari') ;
   }
 }
